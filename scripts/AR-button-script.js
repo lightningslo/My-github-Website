3726058ccd1e8318ed53f3ad5d6b989ae8bfa8da
lightningslo <saml@gustavus.edu>
2023-05-17 10:10:29 -0500

    Doneish
    missing difficult interactive component

--- scripts/AR-button-script.js
+++ scripts/AR-button-script.js
@@ -1,11 +1,28 @@
-    const arButton = document.getElementById('checkbox');
-    const overlay = document.getElementById('overlay')
+const arButton = document.getElementById('checkbox');
+const overlay = document.getElementById('overlay')
     
-    function toggleArTint() {
-      if (overlay.style.visibility == "visible"){
-      overlay.style.visibility = "hidden"
-      }
-      else{
-        overlay.style.visibility = "visible"
-      }
-    };
+function toggleArTint() {
+  if (overlay.style.visibility == "visible"){
+  overlay.style.visibility = "hidden"
+  }
+  else{
+    overlay.style.visibility = "visible"
+  }
+};
+const slideContainer = document.querySelector('.slide-container');
+const slides = Array.from(document.querySelectorAll('.slide'));
+const slideButton = document.querySelector('.slidebutton');
+
+let currentSlideIndex = 0;
+
+slideButton.addEventListener('click', () => {
+  const currentSlide = slides[currentSlideIndex];
+  currentSlide.classList.add('slide-animation');
+  currentSlide.addEventListener('animationend', () => {
+    currentSlide.classList.remove('slide-animation');
+    currentSlide.classList.remove('active-slide');
+    currentSlideIndex = (currentSlideIndex + 1) % slides.length;
+    const nextSlide = slides[currentSlideIndex];
+    nextSlide.classList.add('active-slide');
+  });
+});
