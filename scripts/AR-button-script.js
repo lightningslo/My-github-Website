3cfcb003ae9e64ab1dc71e775b7de5408eb15013
lightningslo <saml@gustavus.edu>
2023-05-17 22:12:58 -0500

    No Sources/References
    Fixed flexboxes and looks cleanish, need those references still but it's not bad.

--- scripts/AR-button-script.js
+++ scripts/AR-button-script.js
@@ -1,12 +1,20 @@
 const arButton = document.getElementById('checkbox');
-const overlay = document.getElementById('overlay')
-    
+const overlay = document.getElementById('overlay');
+const hiddenimg = document.getElementById('hiddenimg');
+const hiddentxt = document.getElementById('hiddentxt');
+
+
+
 function toggleArTint() {
   if (overlay.style.visibility == "visible"){
   overlay.style.visibility = "hidden"
+  hiddenimg.style.visibility = "hidden"
+  hiddentxt.style.visibility = "hidden"
   }
   else{
     overlay.style.visibility = "visible"
+    hiddenimg.style.visibility = "visible"
+    hiddentxt.style.visibility = "visible"
   }
 };
 const slideContainer = document.querySelector('.slide-container');
